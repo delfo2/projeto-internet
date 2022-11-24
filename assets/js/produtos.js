3e965fa3237f1d5866634177bf7563bb0f532436
Carlos Eduardo <105376328+delfo2@users.noreply.github.com>
2022-11-24 19:58:24 -0300

    colocando data attributes no arquivo html adicionando arquivo js

---
+++ assets/js/produtos.js
@@ -0,0 +1,12 @@
+const botoesAddCarrinho = document.querySelectorAll('[data-botao="carrinho"]');
+let teste;
+/*
+botoesAddCarrinho.forEach(element => {
+    element.addEventListener('click', function () {
+        const grupo = this.parentNode.children;
+        const arrayGrupo = Array.from(grupo);
+        console.log(arrayGrupo);
+        teste = arrayGrupo;
+    })
+})
+*/
